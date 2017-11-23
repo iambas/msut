252dc0dc8d3faa12c96d3aed7fa1fbd0077de973
Suphakrit Nakhabat <suphakrit.bas@gmail.com>
2017-11-23 12:37:33 +0700

    change import static and add hamcrest-library

--- app/src/test/java/com/darker/motorservice/StringUtilsTest.java
+++ app/src/test/java/com/darker/motorservice/StringUtilsTest.java
@@ -5,61 +5,58 @@ import com.darker.motorservice.utils.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static com.darker.motorservice.utils.StringUtils.isPhoneNumber;
-import static com.darker.motorservice.utils.StringUtils.stringOk;
-
 /**
  * Created by Darker on 14/11/60.
  */
 
 public class StringUtilsTest {
     @Test
-    public void stringNotNull_isCurrect(){
-        Assert.assertTrue(stringOk("1234"));
+    public void stringNotNull_isCorrect(){
+        Assert.assertTrue(StringUtils.stringOk("1234"));
     }
 
     @Test
-    public void stringNull_isNotCurrect(){
-        Assert.assertFalse(stringOk(null));
+    public void stringNull_isNotCorrect(){
+        Assert.assertFalse(StringUtils.stringOk(null));
     }
 
     @Test
-    public void stringEmpty_isNotCurrect(){
-        Assert.assertFalse(stringOk(""));
+    public void stringEmpty_isNotCorrect(){
+        Assert.assertFalse(StringUtils.stringOk(""));
     }
 
     @Test
-    public void phoneNumber_9digit_isCurrect(){
+    public void phoneNumber_9digit_isCorrect(){
         Assert.assertTrue(StringUtils.isPhoneNumber("123456789"));
     }
 
     @Test
-    public void phoneNumber_10digit_isCurrect(){
-        Assert.assertTrue(isPhoneNumber("1234567890"));
+    public void phoneNumber_10digit_isCorrect(){
+        Assert.assertTrue(StringUtils.isPhoneNumber("1234567890"));
     }
 
     @Test
-    public void phoneNumber_moreThan10digit_isNotCurrect(){
-        Assert.assertFalse(isPhoneNumber("12345678901"));
+    public void phoneNumber_moreThan10digit_isNotCorrect(){
+        Assert.assertFalse(StringUtils.isPhoneNumber("12345678901"));
     }
 
     @Test
-    public void phoneNumber_lessThan9digit_isNotCurrect(){
-        Assert.assertFalse(isPhoneNumber("12345678"));
+    public void phoneNumber_lessThan9digit_isNotCorrect(){
+        Assert.assertFalse(StringUtils.isPhoneNumber("12345678"));
     }
 
     @Test
-    public void phoneNumber_textString_isNotCurrect(){
-        Assert.assertFalse(isPhoneNumber("test"));
+    public void phoneNumber_textString_isNotCorrect(){
+        Assert.assertFalse(StringUtils.isPhoneNumber("test"));
     }
 
     @Test
-    public void phoneNumber_textEmpty_isNotCurrect(){
-        Assert.assertFalse(isPhoneNumber(""));
+    public void phoneNumber_textEmpty_isNotCorrect(){
+        Assert.assertFalse(StringUtils.isPhoneNumber(""));
     }
 
     @Test
-    public void phoneNumberNull_isNotCurrect(){
-        Assert.assertFalse(isPhoneNumber(null));
+    public void phoneNumberNull_isNotCorrect(){
+        Assert.assertFalse(StringUtils.isPhoneNumber(null));
     }
 }
