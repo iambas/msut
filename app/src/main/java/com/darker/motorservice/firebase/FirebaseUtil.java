302207ba8e4e2d20e19d13b2184f8b0c9939b498
Suphakrit Nakhabat <suphakrit.bas@gmail.com>
2017-11-27 10:44:19 +0700

    create method getChild in FirebaseUtil

--- app/src/main/java/com/darker/motorservice/firebase/FirebaseUtil.java
+++ app/src/main/java/com/darker/motorservice/firebase/FirebaseUtil.java
@@ -1,6 +1,8 @@
 package com.darker.motorservice.firebase;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by Darker on 26/11/60.
@@ -10,5 +12,9 @@ public class FirebaseUtil {
 
     public static String getUid(){
         return FirebaseAuth.getInstance().getUid();
+    }
+
+    public static DatabaseReference getChild(String child){
+        return FirebaseDatabase.getInstance().getReference().child(child);
     }
 }
